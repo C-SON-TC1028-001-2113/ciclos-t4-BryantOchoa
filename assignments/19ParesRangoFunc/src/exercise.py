684a6ffb961d5aa1b3a1b5ceb7b4e66cccf844d0
BryantOchoa <A01252419@itesm.mx>
2021-09-07 03:45:38 +0000

    tarea 4 bryant

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,12 @@
 
 def main():
-
+    #escribe tu código abajo de esta línea
+    pass
+    num = int(input("Valor 1: "))
+    num2 = int(input("Valor 2: "))
+    for cont in range(num,num2 + 1):
+        if cont % 2 == 0:
+            print(cont)
 
 if __name__=='__main__':
     main()
